0bd68cd5d93673fe65dafe85ad92c0d0cd3bba29
Gagang <gagangborne@gmail.com>
2017-11-15 15:54:57 +0700

    ketinggalan js-nya

--- poi/js/main.js
+++ poi/js/main.js
@@ -55,19 +55,19 @@ var CRUMINA = {};
             labelField: 'name',
             searchField: ['name'],
             options: [
-                {image: 'img/avatar30-sm.jpg', name: 'Marie Claire Stevens', message:'12 Friends in Common', icon:'olymp-happy-face-icon'},
-                {image: 'img/avatar54-sm.jpg', name: 'Marie Davidson', message:'4 Friends in Common', icon:'olymp-happy-face-icon'},
-                {image: 'img/avatar49-sm.jpg', name: 'Marina Polson', message:'Mutual Friend: Mathilda Brinker', icon:'olymp-happy-face-icon'},
-                {image: 'img/avatar36-sm.jpg', name: 'Ann Marie Gibson', message:'New York, NY', icon:'olymp-happy-face-icon'},
-                {image: 'img/avatar22-sm.jpg', name: 'Dave Marinara', message:'8 Friends in Common', icon:'olymp-happy-face-icon'},
-                {image: 'img/avatar41-sm.jpg', name: 'The Marina Bar', message:'Restaurant / Bar', icon:'olymp-star-icon'}
+                {image: 'img/avatar22-sm.jpg', name: 'Labuan Bajo' },
+                {image: 'img/avatar23-sm.jpg', name: 'Pulau Derawan' },
+                {image: 'img/avatar24-sm.jpg', name: 'Labuan Cermin' },
+                {image: 'img/avatar25-sm.jpg', name: 'Gunung Rinjani' },
+                {image: 'img/avatar26-sm.jpg', name: 'Pulau Komodo' },
+                {image: 'img/avatar27-sm.jpg', name: 'Gunung Bromo' }
             ],
             render: {
                 option: function(item, escape) {
                     return '<div class="inline-items">' +
                         (item.image ? '<div class="author-thumb"><img src="' + escape(item.image) + '" alt="avatar"></div>' : '') +
 							'<div class="notification-event">' +
-                        		(item.name ? '<span class="h6 notification-friend"></a>' + escape(item.name) + '</span>' : '') +
+                        		(item.name ? '<span class="h6"></a>' + escape(item.name) + '</span>' : '') +
                         		(item.message ? '<span class="chat-message-item">' + escape(item.message) + '</span>' : '') +
 							'</div>'+
                         	(item.icon ? '<span class="notification-icon"><svg class="' + escape(item.icon) + '"><use xlink:href="icons/icons.svg#' + escape(item.icon) + '"></use></svg></span>' : '') +
